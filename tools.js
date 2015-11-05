f51c914640456d43c6af787d78cde89914ce5b6e
Alvin1992 <fanrongxin@Alvin.local>
2015-11-05 21:19:41 +0800

    add doMove

--- tools.js
+++ tools.js
@@ -17,3 +17,30 @@ function getStyle(obj, attr) {
 	}
 }
 
+
+
+function doMove(obj, attr, speed, target, fn) {
+
+	clearInterval(obj.timer);
+
+	var cur = parseFloat(getStyle(obj, attr));
+
+	speed = cur > target ? -Math.abs(speed) : speed;
+
+	obj.timer = setInterval(function() {
+
+		cur += speed;
+		//条件speed > 0 && cur >= target || speed < 0 && cur <= target也可以判断
+		if (Math.abs(cur - target) <= Math.abs(speed)) {
+
+			cur = target;
+
+			clearInterval(obj.timer);
+			//先执行了回调函数，从但是后面依然会赋值，因为是两个作用域。
+			fn && fn();
+		}
+
+		obj.style[attr] = cur + 'px';
+
+	}, 30);
+}
