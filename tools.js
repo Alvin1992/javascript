5d52edcf8fb4f94ccbac51c01896481c12a15316
Alvin1992 <fanrongxin@Alvin.local>
2015-11-07 16:20:16 +0800

    add invoke

--- tools.js
+++ tools.js
@@ -44,3 +44,51 @@ function doMove(obj, attr, speed, target, fn) {
 
 	}, 30);
 }
+
+
+function invoke(f, start, interval, end) {
+	if (!start) {start = 0}
+	if ( arguments.length <= 2) {
+		setTimeout(f, start);
+	} else {
+		setTimeout(repeat, start);
+	}
+	function repeat() {
+		var h = setInterval(f, interval);
+		if (end) {setTimeout(function() {clearInterval(h);}, end)};
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
