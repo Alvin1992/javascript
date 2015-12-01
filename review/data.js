0c488a3b8d80213670d0ea0a5fe6fa3c7f77eafa
Alvin1992 <fanrx_1992@163.com>
2015-12-01 18:02:09 +0800

    data

---
+++ review/data.js
@@ -0,0 +1,22 @@
+var arr = [
+	{
+		userName:"leo",
+		age:35,
+		job:"CEO"
+	},
+	{
+		userName:"leo",
+		age:35,
+		job:"CEO"
+	},
+	{
+		userName:"leo",
+		age:35,
+		job:"CEO"
+	},{
+		userName:"leo",
+		age:35,
+		job:"CEO"
+	}
+
+]
